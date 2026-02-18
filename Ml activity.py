4123b09c5e4016d614f170679bf5f732be50ee66
Lokeshconnected <lokeshgowda.m2005@gmail.com>
2026-02-18 12:28:55 +0530

    Clean up code in Ml activity.py
    Removed comments and unnecessary whitespace from the code.

--- Ml activity.py
+++ Ml activity.py
@@ -2,30 +2,13 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix
-
-# Load dataset
 data = pd.read_csv("Iris.csv")
-
-# Input and output
 X = data.drop(["Species", "Id"], axis=1)
 y = data["Species"]
-
-# Split data
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
-
-# Create model
+    X, y, test_size=0.2, random_state=42)
 model = LogisticRegression(max_iter=200)
-
-# Train model
 model.fit(X_train, y_train)
-
-# Predict
 y_pred = model.predict(X_test)
-
-# Accuracy
 print("Accuracy:", accuracy_score(y_test, y_pred))
-
-# Confusion Matrix
 print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
